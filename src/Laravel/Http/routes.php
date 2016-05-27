71bbd3312f7ea5103002994873a8a744d9484f51
Boris Dorofeev <boris.dorofeev2014@yandex.ru>
2016-05-27 12:51:34 +0600

    Рома

--- src/Laravel/Http/routes.php
+++ src/Laravel/Http/routes.php
@@ -2,6 +2,6 @@
 
 Route::group(['middleware' => 'auth', 'prefix' => 'adm'], function()
 {
-
+    Route::post('/smm/save', ['as' => 'smm_save',  'uses' => 'Interpro\SMM\Laravel\Http\SMMController@updateGlobals']);
 });
 
